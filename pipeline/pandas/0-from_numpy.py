0117318a9b7d4d0a311b97b2023abbba90820aad
Idrak <iatakishi@gmail.com>
2026-04-02 17:03:11 +0400

    0 added

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -2,6 +2,8 @@
 """ from numpy """
 import pandas as pd
 
+
 def from_numpy(array):
+    """ from numpy """
     cols = [chr(i) for i in range(65, 65 + array.shape[1])]
     return pd.DataFrame(array, columns=cols)
