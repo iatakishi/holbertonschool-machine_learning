8318a73c868df90489d2abc720e85c1ec7b3a0bf
Idrak <iatakishi@gmail.com>
2026-04-02 17:01:50 +0400

    0 added

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """ from numpy """
 import pandas as pd
-import string
 
 def from_numpy(array):
-    cols = list(string.ascii_uppercase[:array.shape[1]])
+    cols = [chr(i) for i in range(65, 65 + array.shape[1])]
     return pd.DataFrame(array, columns=cols)
