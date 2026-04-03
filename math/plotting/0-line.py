e0b20e840c1cd0b4007bd2c9219f1e28bfd6c368
Idrak <iatakishi@gmail.com>
2026-04-03 21:10:38 +0400

    0 added

---
+++ math/plotting/0-line.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+def line():
+
+    y = np.arange(0, 11) ** 3
+    x = np.arange(0,11)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.xlim(0,10)
+    plt.plot(x,y,'r-')
+    plt.show()
