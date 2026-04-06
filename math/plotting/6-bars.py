8174079b5f0a940c85f2336f63f281499cbedfa8
Idrak <iatakishi@gmail.com>
2026-04-06 15:06:57 +0400

    6 added

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 def bars():
     """ bars """
     np.random.seed(5)
-    fruit = np.random.randint(0, 20, (4,3))
+    fruit = np.random.randint(0, 20, (4, 3))
     x = ['Farrah', 'Fred', 'Felicia']
     apples = fruit[0]
     bananas = fruit[1]
@@ -15,9 +15,12 @@ def bars():
     peaches = fruit[3]
     plt.figure(figsize=(6.4, 4.8))
     plt.bar(x, apples, label='apples', width=0.5, color='red')
-    plt.bar(x, bananas, bottom=apples, label='bananas', width=0.5, color='yellow')
-    plt.bar(x, oranges, bottom=apples+bananas, label='oranges', width=0.5, color='#ff8000')
-    plt.bar(x, peaches, bottom=apples+bananas+oranges, label='peaches', width=0.5, color='#ffe5b4')
+    plt.bar(x, bananas, bottom=apples,
+            label='bananas',width=0.5, color='yellow')
+    plt.bar(x, oranges, bottom=apples+bananas,
+            label='oranges', width=0.5, color='#ff8000')
+    plt.bar(x, peaches, bottom=apples+bananas+oranges,
+            label='peaches', width=0.5, color='#ffe5b4')
     plt.title('Number of Fruit per Person')
     plt.ylabel('Quantity of Fruit')
     plt.ylim(0, 80)
