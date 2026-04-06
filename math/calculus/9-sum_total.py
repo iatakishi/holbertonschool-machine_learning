6e4c820bbdce52f1eee6f1172c13e945dec12784
Idrak <iatakishi@gmail.com>
2026-04-06 23:19:26 +0400

    new answer added

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,4 +4,7 @@
 
 def summation_i_squared(n):
     """ sum total """
-    return sum(i**2 for i in range(1, n+1))
+    if isinstance(n, int):
+        return sum(i**2 for i in range(1, n+1))
+    else:
+        return None
