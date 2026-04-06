8fd840b6797054059ca290cc1dda854aef06967b
Idrak <iatakishi@gmail.com>
2026-04-06 23:13:39 +0400

    new answer added

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" sum total """
+
+
+def summation_i_squared(n):
+    """ sum total """
+    total = 0
+    for i in range(n):
+        total = total + i**2
+    return total
