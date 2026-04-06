bbf658288b1e073f451a2f14adc39ded115adb81
Idrak <iatakishi@gmail.com>
2026-04-06 12:05:03 +0400

    4 added

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" frequency """
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """ frequency """
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.hist(student_grades, bins=range(min(student_grades), max(student_grades) + 10, 10), edgecolor='black')
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.title('Project A')
+    plt.show()
