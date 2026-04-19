0447d148d76fe9f9e191797f953903e4ce8a6a78
Idrak <iatakishi@gmail.com>
2026-04-19 23:38:43 +0400

    2 added

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" matrix shape """
+
+
+def matrix_shape(matrix):
+        """ matrix shape """
+        shape = []
+        while isinstance(matrix, list):
+                shape.append(len(matrix))
+                matrix = matrix[0]
+        return shape
