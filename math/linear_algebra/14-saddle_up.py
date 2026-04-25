e0f8684fbf23890ca2982e4671e878835d3371aa
Idrak <iatakishi@gmail.com>
2026-04-26 00:38:02 +0400

    14 added

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module containing np_matmul function"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Performs matrix multiplication of two numpy.ndarrays"""
+    return np.matmul(mat1, mat2)
