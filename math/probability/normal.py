288136eecc99ceeb34c74bb6efb93c03c8e662e1
Idrak <iatakishi@gmail.com>
2026-05-09 01:58:48 +0400

    6 added

---
+++ math/probability/normal.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" normal """
+
+
+class Normal:
+    """ normal """
+    def __init__(self, data=None, mean=0., stddev=1.):
+        if data is not None:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("""data must contain multiple values""")
+            self.mean = sum(data) / len(data)
+            deviations = []
+            for i in data:
+                deviations.append((i - self.mean)**2)
+            self.stddev = (sum(deviations) / len(data)) ** 0.5
+        else:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            self.mean = float(mean)
+            self.stddev = float(stddev)
