5156d17e36e33730fef57dc29c42d16fe82be4d8
Idrak <iatakishi@gmail.com>
2026-05-09 00:05:03 +0400

    3 added

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -14,4 +14,4 @@ class Exponential:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = sum(data) / len(data)
+            self.lambtha = 1 / (sum(data) / len(data))
