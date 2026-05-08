4ecbc39abcf5b76a2bed2a2342181fe7d41efcfd
Idrak <iatakishi@gmail.com>
2026-05-09 00:14:23 +0400

    4 added

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -15,3 +15,11 @@ class Exponential:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = 1 / (sum(data) / len(data))
+
+    def pdf(self, x):
+        """ pdf """
+        if x < 0:
+            return 0
+        e = 2.7182818285
+        P = (self.lambtha * e) ** -(self.lambtha * x)
+        return P
