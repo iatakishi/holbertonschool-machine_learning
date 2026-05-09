e1e6469f095ef9e5f1c019b32adaa3828101e34c
Idrak <iatakishi@gmail.com>
2026-05-09 13:36:02 +0400

    9 added

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -46,7 +46,9 @@ class Normal:
         def erf(x):
             """ erf """
             erf = (2 / (pi ** 0.5)) * (x - (x ** 3 / 3) +
-                                       (x ** 5 / 10) - (x ** 7 / 42) + (x ** 9 / 216))
+                                       (x ** 5 / 10) -
+                                       (x ** 7 / 42) +
+                                       (x ** 9 / 216))
             return erf
         P = 0.5 * (1 + erf((x - self.mean) / (self.stddev * 2 ** 0.5)))
         return P
