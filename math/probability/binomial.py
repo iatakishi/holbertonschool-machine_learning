4dacab316e2ededfc66b0a52d32a13aedb08ec0d
Idrak <iatakishi@gmail.com>
2026-05-09 14:56:29 +0400

    10 added

---
+++ math/probability/binomial.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" binomial """
+
+
+class Binomial:
+    """ binomial """
+    def __init__(self, data=None, n=1, p=0.5):
+        if data is not None:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("""data must contain multiple values""")
+            mean = sum(data) / len(data)
+            variance = sum((i - mean) ** 2 for i in data) / len(data)
+            p = 1 - (variance / mean)
+            n = round (mean / p)
+            p = mean / n
+            self.n = n
+            self.p = p
+        else:
+            if n <= 0:
+                raise ValueError("n must be a positive value")
+            if p <= 0 or p >= 1:
+                raise ValueError("p must be greater than 0 and less than 1")
+            self.n = int(n)
+            self.p = float(p)
