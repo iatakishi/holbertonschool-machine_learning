9c1d8162958aa77779ce4de3fc852c17fc9349cb
Idrak <iatakishi@gmail.com>
2026-05-09 12:44:25 +0400

    8 added

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -30,3 +30,9 @@ class Normal:
         """ x_value """
         x = self.mean + z * self.stddev
         return x
+
+    def pdf(self, x):
+        pi = 3.1415926536
+        e = 2.7182818285
+        P = (1 / (self.stddev * (2 * pi) ** 0.5)) * e ** (-(x - self.mean) ** 2 / (2 * self.stddev ** 2))
+        return P
