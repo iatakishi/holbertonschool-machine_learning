76b4a7245ccefe6f8af5a6f077cf2a7dd0969e44
Idrak <iatakishi@gmail.com>
2026-05-11 17:48:50 +0400

    1 added

---
+++ supervised_learning/decision_tree/1-build_decision_tree.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+""" decision tree """
+import numpy as np
+
+
+class Node:
+    """ node """
+    def __init__(self, feature=None, threshold=None, left_child=None,
+                 right_child=None, is_root=False, depth=0):
+        self.feature = feature
+        self.threshold = threshold
+        self.left_child = left_child
+        self.right_child = right_child
+        self.is_leaf = False
+        self.is_root = is_root
+        self.sub_population = None
+        self.depth = depth
+
+    def max_depth_below(self):
+        """ max depth below """
+        return max(self.left_child.max_depth_below(),
+                   self.right_child.max_depth_below())
+
+
+class Leaf(Node):
+    """ leaf """
+    def __init__(self, value, depth=None):
+        super().__init__()
+        self.value = value
+        self.is_leaf = True
+        self.depth = depth
+
+    def max_depth_below(self):
+        """ max depth below """
+        return self.depth
+
+    def count_nodes_below(self, only_leaves=False):
+        return 1
+
+
+class Decision_Tree():
+    """ decision tree"""
+    def __init__(self, max_depth=10, min_pop=1, seed=0,
+                 split_criterion="random", root=None):
+        self.rng = np.random.default_rng(seed)
+        if root:
+            self.root = root
+        else:
+            self.root = Node(is_root=True)
+        self.explanatory = None
+        self.target = None
+        self.max_depth = max_depth
+        self.min_pop = min_pop
+        self.split_criterion = split_criterion
+        self.predict = None
+
+    def depth(self):
+        """ depth """
+        return self.root.max_depth_below()
+
+    def count_nodes(self, only_leaves=False):
+        return self.root.count_nodes_below(only_leaves=only_leaves)
