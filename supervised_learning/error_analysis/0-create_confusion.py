0e574b8f3234e1c2a535f47d2d9b53fcea985d07
Idrak <iatakishi@gmail.com>
2026-05-11 15:48:41 +0400

    relocate the file

---
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Creates a confusion matrix"""
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+    """Creates a confusion matrix.
+
+    Args:
+        labels: one-hot numpy.ndarray of shape (m, classes) with correct labels
+        logits: one-hot numpy.ndarray of shape (m, classes) with predicted labels
+
+    Returns:
+        confusion matrix of shape (classes, classes) where rows = correct labels,
+        columns = predicted labels
+    """
+    return np.matmul(labels.T, logits)
