d457c0d3a44f3c3450a07a8ea68772c9d57e6480
Idrak <iatakishi@gmail.com>
2026-05-11 15:49:57 +0400

    0 added

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -7,11 +7,14 @@ def create_confusion_matrix(labels, logits):
     """Creates a confusion matrix.
 
     Args:
-        labels: one-hot numpy.ndarray of shape (m, classes) with correct labels
-        logits: one-hot numpy.ndarray of shape (m, classes) with predicted labels
+        labels: one-hot numpy.ndarray of shape
+        (m, classes) with correct labels
+        logits: one-hot numpy.ndarray of shape
+        (m, classes) with predicted labels
 
     Returns:
-        confusion matrix of shape (classes, classes) where rows = correct labels,
+        confusion matrix of shape (classes, classes)
+        where rows = correct labels,
         columns = predicted labels
     """
     return np.matmul(labels.T, logits)
