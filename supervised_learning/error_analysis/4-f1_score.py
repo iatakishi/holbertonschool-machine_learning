a0a35946aedcbbb395e46616fa1a10fce67bf3dd
Idrak <iatakishi@gmail.com>
2026-05-11 16:58:56 +0400

    4 added

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -2,31 +2,12 @@
 """ f1 score """
 import numpy as np
 
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
+
 
 def f1_score(confusion):
     """ f1 score """
-    # True Positives: diagonal elements
-    tp = np.diag(confusion)
-
-    # Total samples in the matrix
-    total = confusion.sum()
-
-    # Actual positives per class (row sums)
-    actual_pos = confusion.sum(axis=1)
-
-    # Predicted positives per class (column sums)
-    predicted_pos = confusion.sum(axis=0)
-
-    # False Positives: predicted as class i but actually something else
-    fp = predicted_pos - tp
-
-    # True Negatives: everything not in the actual positive row & not FP
-    tn = total - actual_pos - fp
-
-    precision = tp / predicted_pos
-
-    recall = tp / actual_pos
-
-    f1 = 2 * (precision + recall) / (precision + recall)
-
-    return f1
+    prec = precision(confusion)
+    rec = sensitivity(confusion)
+    return 2 * prec * rec / (prec + rec)
