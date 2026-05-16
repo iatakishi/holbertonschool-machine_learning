914a0e0d4a84b7c18c09324c82f373f5e60cf12c
Idrak <iatakishi@gmail.com>
2026-05-16 16:17:10 +0400

    2 added

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -30,7 +30,7 @@ class Node:
     def left_child_add_prefix(self, text):
         """ left child add prefix """
         lines = text.split("\n")
-        new_text = "    +--" + lines[0] + "\n"
+        new_text = "    +---> " + lines[0] + "\n"
         for x in lines[1:]:
             new_text += ("    |  " + x) + "\n"
         return new_text
@@ -38,7 +38,7 @@ class Node:
     def right_child_add_prefix(self, text):
         """ right child add prefix """
         lines = text.split("\n")
-        new_text = "    +--" + lines[0] + "\n"
+        new_text = "    +---> " + lines[0] + "\n"
         for x in lines[1:]:
             new_text += ("       " + x) + "\n"
         return new_text
