5e953ccde93c148b324f3bfdce2698673f33e0f1
Idrak <iatakishi@gmail.com>
2026-05-16 16:02:46 +0400

    2 added

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -27,6 +27,32 @@ class Node:
         right_count = self.right_child.count_nodes_below(only_leaves)
         return (0 if only_leaves else 1) + left_count + right_count
 
+    def left_child_add_prefix(self, text):
+        """ left child add prefix """
+        lines = text.split("\n")
+        new_text = "    +--" + lines[0] + "\n"
+        for x in lines[1:]:
+            new_text += ("    |  " + x) + "\n"
+        return new_text
+
+    def right_child_add_prefix(self, text):
+        """ right child add prefix """
+        lines = text.split("\n")
+        new_text = "    +--" + lines[0] + "\n"
+        for x in lines[1:]:
+            new_text += ("       " + x) + "\n"
+        return new_text
+
+    def __str__(self):
+        """ str """
+        if self.is_root:
+            t = f"root [feature={self.feature}, threshold={self.threshold}]"
+        else:
+            t = f"node [feature={self.feature}, threshold={self.threshold}]"
+        t += "\n" + self.left_child_add_prefix(self.left_child.__str__())
+        t += self.right_child_add_prefix(self.right_child.__str__())
+        return t
+
 
 class Leaf(Node):
     """ leaf """
@@ -41,41 +67,16 @@ class Leaf(Node):
         return self.depth
 
     def count_nodes_below(self, only_leaves=False):
-        """ count_nodes_below """
+        """ count nodes below """
         return 1
-
-    def left_child_add_prefix(self, text):
-        """ left_child_add_prefix """
-        lines = text.split("\n")
-        new_text = "    +--->" + lines[0] + "\n"
-        for x in lines[1:]:
-            new_text += "    |   " + x + "\n"
-        return new_text
-
-    def right_child_add_prefix(self, text):
-        """ right_child_add_prefix """
-        lines = text.split("\n")
-        new_text = "         +--->" + lines[0] + "\n"
-        for x in lines[1:]:
-            new_text += "            " + x + "\n"
-        return new_text
 
     def __str__(self):
         """ str """
-        if self.is_root:
-            t = f"root [feature={self.feature}, threshold={self.threshold}]"
-        else:
-            t = f"node [feature={self.feature}, threshold={self.threshold}]"
-
-        if self.left_child:
-            t += "\n" + self.left_child_add_prefix(self.left_child.__str__())
-        if self.right_child:
-            t += "\n" + self.right_child_add_prefix(self.right_child.__str__())
-        return t
+        return f"-> leaf [value={self.value}]"
 
 
 class Decision_Tree():
-    """ decision tree"""
+    """ decision tree """
     def __init__(self, max_depth=10, min_pop=1, seed=0,
                  split_criterion="random", root=None):
         self.rng = np.random.default_rng(seed)
@@ -95,5 +96,9 @@ class Decision_Tree():
         return self.root.max_depth_below()
 
     def count_nodes(self, only_leaves=False):
-        """ count_nodes """
+        """ count nodes """
         return self.root.count_nodes_below(only_leaves=only_leaves)
+
+    def __str__(self):
+        """ str """
+        return self.root.__str__()
