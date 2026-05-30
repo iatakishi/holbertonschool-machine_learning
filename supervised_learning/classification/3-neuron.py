d1a291db2579a15554bb5f401f7b4ef024bc9ec1
Idrak <iatakishi@gmail.com>
2026-05-30 23:22:48 +0400

    3 added

---
+++ supervised_learning/classification/3-neuron.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""Module for Neuron class"""
+import numpy as np
+
+
+# Neuron performing binary classification with cost calculation
+class Neuron:
+    """Defines a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """Initialize neuron with nx input features"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for weights vector"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for bias"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for activated output"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """Calculate forward propagation using sigmoid activation"""
+        Z = np.dot(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """Calculate logistic regression cost"""
+        m = Y.shape[1]
+        cost = -np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)) / m
+        return cost
