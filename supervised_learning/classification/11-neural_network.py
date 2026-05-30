86231d2a8ac34585d90ad06a64a2229ea28a1c67
Idrak <iatakishi@gmail.com>
2026-05-30 23:56:32 +0400

    11 added

---
+++ supervised_learning/classification/11-neural_network.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+"""Module for NeuralNetwork class"""
+import numpy as np
+
+
+# Neural network with one hidden layer and cost calculation
+class NeuralNetwork:
+    """Defines a neural network with one hidden layer performing binary classification"""
+
+    def __init__(self, nx, nodes):
+        """Initialize neural network with nx inputs and nodes in hidden layer"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """Getter for hidden layer weights"""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """Getter for hidden layer bias"""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """Getter for hidden layer activated output"""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """Getter for output neuron weights"""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """Getter for output neuron bias"""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """Getter for output neuron activated output"""
+        return self.__A2
+
+    def forward_prop(self, X):
+        """Calculate forward propagation through both layers"""
+        Z1 = np.dot(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-Z1))
+        Z2 = np.dot(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-Z2))
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """Calculate logistic regression cost"""
+        m = Y.shape[1]
+        cost = -np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)) / m
+        return cost
