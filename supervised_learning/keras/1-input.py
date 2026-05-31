1f335747da36bc17651baf02ed9e53767cd6c297
Idrak <iatakishi@gmail.com>
2026-05-31 14:02:31 +0400

    1 added

---
+++ supervised_learning/keras/1-input.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Module for building a Keras neural network model using Input"""
+import tensorflow.keras as K
+
+
+# Build a neural network model using Keras functional API
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """Build a neural network with L2 regularization and dropout"""
+    reg = K.regularizers.L2(lambtha)
+    inputs = K.Input(shape=(nx,))
+    x = K.layers.Dense(layers[0], activation=activations[0],
+                        kernel_regularizer=reg)(inputs)
+    for i in range(1, len(layers)):
+        x = K.layers.Dropout(1 - keep_prob)(x)
+        x = K.layers.Dense(layers[i], activation=activations[i],
+                            kernel_regularizer=reg)(x)
+    return K.Model(inputs=inputs, outputs=x)
