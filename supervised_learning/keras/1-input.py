0d0b54b90c0b64dfe3e36d9c8957da2b70e22ad3
Idrak <iatakishi@gmail.com>
2026-05-31 14:05:16 +0400

    1 added

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -8,8 +8,10 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     """Build a neural network with L2 regularization and dropout"""
     reg = K.regularizers.L2(lambtha)
     inputs = K.Input(shape=(nx,))
-    x = K.layers.Dense(layers[0], activation=activations[0], kernel_regularizer=reg)(inputs)
+    x = K.layers.Dense(layers[0], activation=activations[0],
+                       kernel_regularizer=reg)(inputs)
     for i in range(1, len(layers)):
         x = K.layers.Dropout(1 - keep_prob)(x)
-        x = K.layers.Dense(layers[i], activation=activations[i], kernel_regularizer=reg)(x)
+        x = K.layers.Dense(layers[i], activation=activations[i],
+                           kernel_regularizer=reg)(x)
     return K.Model(inputs=inputs, outputs=x)
