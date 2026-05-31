3ba516f1ee0c221dbf4d6ba36f6a527b6054ad5d
Idrak <iatakishi@gmail.com>
2026-05-31 14:59:11 +0400

    5 added

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module for training a Keras model with validation"""
+import tensorflow.keras as K
+
+
+# Train a Keras model with optional validation data
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+    """Train a model using mini-batch gradient descent with optional validation"""
+    return network.fit(data, labels,
+                       batch_size=batch_size,
+                       epochs=epochs,
+                       verbose=verbose,
+                       shuffle=shuffle,
+                       validation_data=validation_data)
