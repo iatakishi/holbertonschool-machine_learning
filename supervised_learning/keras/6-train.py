5e5a280d1d1d7b81b1131bea519031e9f797e510
Idrak <iatakishi@gmail.com>
2026-05-31 15:05:53 +0400

    6 added

---
+++ supervised_learning/keras/6-train.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Module for training a Keras model with early stopping"""
+import tensorflow.keras as K
+
+
+# Train a Keras model with optional early stopping
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False,
+                patience=0, verbose=True, shuffle=False):
+    """Train a model using
+    mini-batch gradient descent with early stopping"""
+    callbacks = []
+    if early_stopping and validation_data:
+        callbacks.append(K.callbacks.EarlyStopping(monitor='val_loss',
+                                                    patience=patience))
+    return network.fit(data, labels,
+                       batch_size=batch_size,
+                       epochs=epochs,
+                       verbose=verbose,
+                       shuffle=shuffle,
+                       validation_data=validation_data,
+                       callbacks=callbacks)
