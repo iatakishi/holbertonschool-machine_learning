749f713edaf2c01a933c9e9412cff54aca35afb6
Idrak <iatakishi@gmail.com>
2026-06-01 01:05:38 +0400

    13 added

--- supervised_learning/optimization/13-batch_norm.py
+++ supervised_learning/optimization/13-batch_norm.py
@@ -5,7 +5,8 @@ import numpy as np
 
 # Normalize an unactivated output using batch normalization
 def batch_norm(Z, gamma, beta, epsilon):
-    """Normalize an unactivated output of a neural network using batch normalization"""
+    """Normalize an unactivated
+    output of a neural network using batch normalization"""
     mean = Z.mean(axis=0)
     var = Z.var(axis=0)
     Z_norm = (Z - mean) / np.sqrt(var + epsilon)
