29654a49dc7e191797b08cbe17674d2a156a8090
Idrak <iatakishi@gmail.com>
2026-06-06 17:55:45 +0400

    6 added

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -1,25 +1,36 @@
 #!/usr/bin/env python3
-"""Module for creating a layer with dropout."""
-
+"""
+Creates a layer of a neural network using dropout.
+"""
 import tensorflow as tf
 
 
 def dropout_create_layer(prev, n, activation, keep_prob, training=True):
     """
-    Creates a layer of a neural network using dropout.
+    Creates a layer of a neural network using dropout:
 
-    Arguments:
-    prev -- tensor containing the output of the previous layer
-    n -- number of nodes the new layer should contain
-    activation -- activation function for the new layer
-    keep_prob -- probability that a node will be kept
-    training -- boolean indicating
-    if the model is in training mode
+    * prev: A tensor containing the output of the previous layer
+    * n: The number of nodes the new layer should contain
+    * activation: The activation function for the new layer
+    * keep_prob: The probability that a node will be kept
+    * training: A boolean indicating whether the model is in training mode
 
-    Returns:
-    the output of the new layer
+    Returns: The output of the new layer
     """
-    dense = tf.keras.layers.Dense(units=n, activation=activation)
-    dropout = tf.keras.layers.Dropout(rate=1 - keep_prob)
+    # Standard initialization for Holberton ML tasks
+    initializer = tf.keras.initializers.VarianceScaling(scale=2.0, mode="fan_avg")
 
-    return dropout(dense(prev), training=training)
+    # Create the dense layer
+    dense_layer = tf.keras.layers.Dense(units=n,
+                                        activation=activation,
+                                        kernel_initializer=initializer)
+
+    # Generate the output from the dense layer
+    layer_output = dense_layer(prev)
+
+    # Create the Dropout layer. Note: Keras Dropout expects 'rate' (fraction to drop),
+    # so we must convert keep_prob to rate by subtracting it from 1.0.
+    dropout_layer = tf.keras.layers.Dropout(rate=1.0 - keep_prob)
+
+    # Apply dropout to the output, explicitly passing the training flag
+    return dropout_layer(layer_output, training=training)
