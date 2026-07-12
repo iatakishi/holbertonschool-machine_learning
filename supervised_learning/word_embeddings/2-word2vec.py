205493900cf53d54a0788da2d696e85bc121903f
Idrak <iatakishi@gmail.com>
2026-07-12 22:53:53 +0400

    task is added

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-""" Word2Vec Model Module """
 import gensim
-
+""" Word2Vec Model Module """
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
                    negative=5, cbow=True, epochs=5, seed=0, workers=1):
