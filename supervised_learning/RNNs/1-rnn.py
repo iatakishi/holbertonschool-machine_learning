75d629883e91ce45f9d59c5b85216afa0dccc5ed
Idrak <iatakishi@gmail.com>
2026-07-12 15:48:32 +0400

    task is added

--- supervised_learning/RNNs/1-rnn.py
+++ supervised_learning/RNNs/1-rnn.py
@@ -10,12 +10,15 @@ def rnn(rnn_cell, X, h_0):
     Performs forward propagation for a simple RNN
 
     Parameters:
-    rnn_cell is an instance of RNNCell that will be used for the forward prop
-    X is the data to be used, given as a numpy.ndarray of shape (t, m, i)
+    rnn_cell is an instance of RNNCell
+    that will be used for the forward prop
+    X is the data to be used, given
+    as a numpy.ndarray of shape (t, m, i)
         t is the maximum number of time steps
         m is the batch size
         i is the dimensionality of the data
-    h_0 is the initial hidden state, given as a numpy.ndarray of shape (m, h)
+    h_0 is the initial hidden state,
+    given as a numpy.ndarray of shape (m, h)
         h is the dimensionality of the hidden state
 
     Returns: H, Y
@@ -25,11 +28,13 @@ def rnn(rnn_cell, X, h_0):
     t, m, _ = X.shape
     _, h = h_0.shape
 
-    # We can infer the output dimensionality (o) from the bias of the cell
+    # We can infer the output dimensionality
+    # (o) from the bias of the cell
     o = rnn_cell.by.shape[1]
 
     # Initialize the H and Y arrays with zeros
-    # H has shape (t + 1, m, h) to include the initial hidden state h_0
+    # H has shape (t + 1, m, h) to include
+    # the initial hidden state h_0
     H = np.zeros((t + 1, m, h))
     Y = np.zeros((t, m, o))
 
