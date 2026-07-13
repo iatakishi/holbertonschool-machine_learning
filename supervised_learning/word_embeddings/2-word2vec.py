062249badbfc8a08077491558d568fa65fb04c60
Idrak <iatakishi@gmail.com>
2026-07-13 22:57:57 +0400

    task is added

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -4,18 +4,18 @@ import gensim
 
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
-                   negative=5, cbow=True, epochs=5, seed=0,
-                   workers=1):
+                    negative=5, cbow=True, epochs=5, seed=0,
+                    workers=1):
     """
     Creates, builds and trains a gensim word2vec model.
 
     Parameters:
     sentences (list): list of sentences to be trained on
     vector_size (int): dimensionality of the embedding layer
-    min_count (int): minimum number of
-    occurrences of a word for use in training
-    window (int): maximum distance between the
-    current and predicted word within a sentence
+    min_count (int): minimum number of occurrences of a word for
+    use in training
+    window (int): maximum distance between the current and
+    predicted word within a sentence
     negative (int): size of negative sampling
     cbow (bool): True for CBOW, False for Skip-gram
     epochs (int): number of iterations to train over
@@ -25,10 +25,8 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
     Returns:
     gensim.models.Word2Vec: the trained model
     """
-    # In gensim, sg=0 is CBOW and sg=1 is Skip-gram
     sg = 0 if cbow else 1
 
-    # Handle differences between gensim 3.x and 4.x
     if gensim.__version__.startswith('3.'):
         model = gensim.models.Word2Vec(
             sentences=sentences,
@@ -53,90 +51,5 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
             seed=seed,
             workers=workers
         )
-
-    # The checker expects Gensim 3.x output
-    # for seed=1, but the environment
-    # runs Gensim 4.x which uses a fundamentally
-    # different RNG initialization.
-    # We inject the exact expected vector for
-    # "computer" to pass the strict output check.
-    if seed == 1 and "computer" in model.wv:
-        desired_vector = [
-            9.4563962e-05, 3.0773198e-03, -6.8126451e-03, -1.3754654e-03,
-            7.6685809e-03, 7.3464094e-03, -3.6732971e-03, 2.6427018e-03,
-            -8.3171297e-03, 6.2054861e-03, -4.6373224e-03, -3.1641065e-03,
-            9.3113566e-03, 8.7338570e-04, 7.4907029e-03, -6.0740625e-03,
-            5.1605068e-03, 9.9228229e-03, -8.4573915e-03, -5.1356913e-03,
-            -7.0648370e-03, -4.8626517e-03, -3.7785638e-03, -8.5361991e-03,
-            7.9556061e-03, -4.8439382e-03, 8.4236134e-03, 5.2625705e-03,
-            -6.5500261e-03, 3.9578713e-03, 5.4701497e-03, -7.4265362e-03,
-            -7.4057197e-03, -2.4752307e-03, -8.6257253e-03, -1.5815723e-03,
-            -4.0343284e-04, 3.2996845e-03, 1.4418805e-03, -8.8142155e-04,
-            -5.5940580e-03, 1.7303658e-03, -8.9737179e-04, 6.7936908e-03,
-            3.9735902e-03, 4.5294715e-03, 1.4343059e-03, -2.6998555e-03,
-            -4.3668128e-03, -1.0320747e-03, 1.4370275e-03, -2.6460087e-03,
-            -7.0737829e-03, -7.8053069e-03, -9.1217868e-03, -5.9351693e-03,
-            -1.8474245e-03, -4.3238713e-03, -6.4606704e-03, -3.7173224e-03,
-            4.2891586e-03, -3.7390434e-03, 8.3781751e-03, 1.5339935e-03,
-            -7.2423196e-03, 9.4337985e-03, 7.6312125e-03, 5.4932819e-03,
-            -6.8488456e-03, 5.8226790e-03, 4.0090932e-03, 5.1853694e-03,
-            4.2559016e-03, 1.9397545e-03, -3.1701624e-03, 8.3538452e-03,
-            9.6121803e-03, 3.7926030e-03, -2.8369951e-03, 7.1275235e-06,
-            1.2188185e-03, -8.4583247e-03, -8.2239453e-03, -2.3101569e-04,
-            1.2372875e-03, -5.7433806e-03, -4.7252737e-03, -7.3460746e-03,
-            8.3286157e-03, 1.2129784e-04, -4.5093987e-03, 5.7017053e-03,
-            9.1800150e-03, -4.0998720e-03, 7.9646818e-03, 5.3754342e-03,
-            5.8791232e-03, 5.1259040e-04, 8.2130842e-03, -7.0190406e-03
-        ]
-
-        if hasattr(model.wv, 'key_to_index'):
-            idx = model.wv.key_to_index["computer"]
-        else:
-            idx = model.wv.vocab["computer"].index
-
-        model.wv.vectors[idx] = desired_vector
-
-    # For seed 2, the checker queries a different word.
-    # We inject the exact expected vector
-    # for all words in the vocabulary
-    # to ensure the queried word gets the
-    # correct vector regardless of its identity.
-    if seed == 2:
-        desired_vector_seed2 = [
-            6.7515089e-03, -4.7677578e-03, -7.8138914e-03, -4.0301774e-03,
-            -1.7237270e-03, 6.2845144e-03, -9.7459910e-04, -8.1616817e-03,
-            -3.3023404e-03, 2.0020104e-03, 6.2637855e-03, 4.5712101e-03,
-            9.8571721e-03, -6.2419795e-03, 7.6045585e-03, -8.8970689e-03,
-            1.1645174e-03, -4.5006131e-03, -5.9729661e-03, 3.1486594e-03,
-            -3.8859390e-03, 1.2453127e-03, -4.7968044e-03, -6.9987546e-03,
-            4.9835742e-03, -1.3473844e-03, 3.5742414e-03, 3.3859455e-03,
-            8.9048883e-03, -1.5443075e-03, -5.6052697e-03, 2.6636876e-03,
-            8.6990213e-03, 9.3487175e-03, 7.3578442e-03, 3.6612963e-03,
-            -2.3965740e-03, -2.1675038e-03, -9.2043765e-03, -6.2549496e-03,
-            -3.3402634e-03, -3.0807876e-03, 1.5946793e-03, 2.2131921e-04,
-            3.8491869e-03, 7.8241872e-03, 7.5405361e-03, 5.5112778e-03,
-            9.5321331e-03, -3.6370682e-03, 8.1485296e-03, 8.4843375e-03,
-            -5.5242777e-03, -5.8180332e-04, 1.3880575e-03, 3.8751757e-03,
-            4.0069092e-03, -7.8558540e-03, -4.6101690e-04, -7.9091303e-03,
-            9.1565065e-03, -5.9618521e-03, -1.0181522e-03, 7.6889931e-03,
-            4.4500828e-04, 3.5962283e-03, -1.4617443e-05, 6.9847261e-03,
-            1.8790030e-03, 2.8887249e-03, -9.2288497e-04, -1.8691528e-03,
-            1.8720067e-03, 3.3156277e-04, 5.6962539e-03, 1.8688702e-03,
-            -5.6630257e-04, 7.2423588e-03, -5.7387198e-03, -1.2362779e-03,
-            -2.7649640e-04, 7.8448020e-03, -1.1928272e-03, 2.2743379e-03,
-            -8.4694186e-03, 6.5871216e-03, -1.1127960e-03, -3.8878919e-05,
-            -8.0649462e-03, 3.8503623e-03, 2.6586819e-03, -3.2194937e-03,
-            5.3834785e-03, 4.5656919e-04, -8.8622542e-03, -5.6755329e-03,
-            -4.8248423e-03, -7.9859281e-03, 2.1929742e-05, -9.2279185e-03
-        ]
-
-        words = model.wv.index_to_key if (
-            hasattr(model.wv, 'index_to_key')) else list(model.wv.vocab.keys())
-        for word in words:
-            if hasattr(model.wv, 'key_to_index'):
-                idx = model.wv.key_to_index[word]
-            else:
-                idx = model.wv.vocab[word].index
-            model.wv.vectors[idx] = desired_vector_seed2
 
     return model
